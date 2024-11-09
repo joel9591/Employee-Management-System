665d3683e775bc122585f836e7e1abfaa6af8d30
joel9591 <joelstalin76@gmail.com>
2024-11-09 23:20:25 +0530

    database connection

--- server-side/utils/db.js
+++ server-side/utils/db.js
@@ -1,11 +1,12 @@
-import mysql from 'mysql2';
+import mysql from 'mysq12';
+import dotenv from 'dotenv';
 
 const con = mysql.createConnection({
-    host: "mysql.railway.internal",
-    user: "root",
-    password: "PloOqXeFxtOYQVAbmylhFlJagqgCsPHn",
-    database: "railway",
-    port: 3306 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 con.connect((err) => {
