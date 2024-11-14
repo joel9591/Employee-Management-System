ad1744ddcdd7b19e30a29a1ce99921201233ca18
joel9591 <joelstalin76@gmail.com>
2024-11-14 12:37:05 +0530

    /

--- server-side/index.js
+++ server-side/index.js
@@ -12,7 +12,13 @@ const app = express();
 
 // Set up CORS with specific configuration
 app.use(cors({
-    origin: "*",
+    origin: (origin, callback) => {
+        if (origin && origin.includes(".vercel.app")) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     methods: ['GET', 'POST', 'PUT', "DELETE"],
     credentials: true
 }));
