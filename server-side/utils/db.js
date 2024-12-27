f5c73008c1d732da9b091c2d2eb19ebe2d7ab7a9
joel9591 <joelstalin76@gmail.com>
2024-12-28 01:31:02 +0700

    redeployed database

--- server-side/utils/db.js
+++ server-side/utils/db.js
@@ -1,12 +1,14 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
 const con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    //port: process.env.DB_PORT
+    port: process.env.DB_PORT
 });
 
 con.connect((err) => {
