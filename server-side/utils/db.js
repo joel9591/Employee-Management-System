21ef5a6706073b10d22cfa9e406862c30fada96a
joel9591 <joelstalin76@gmail.com>
2024-12-28 20:24:37 +0700

    configured DB

--- server-side/utils/db.js
+++ server-side/utils/db.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 const con = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    database: process.env.DB_NAME
 });
 
 con.connect((err) => {
@@ -18,6 +18,5 @@ con.connect((err) => {
         console.log("Connected to the database");
     }
 });
-
 
 export default con;
